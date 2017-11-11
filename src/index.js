d19a260081dd06373d1237ad1498b84d7cbdf6a6
Kelvin Goodman <kelvin12goodman@gmail.com>
2017-11-11 11:57:19 -0500

    moved state up to board from child squares

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={() => alert("Hey Kelvin"+ this.props.value)}>
+      <button className="square" onClick={() => this.props.onClick()}>
         {this.props.value}
       </button>
     );
@@ -12,8 +12,19 @@ class Square extends React.Component {
 }
 
 class Board extends React.Component {
+	constructor(props) {
+    super(props);
+    this.state = {
+      squares: Array(9).fill(null),
+    };
+  }
   renderSquare(i) {
-    return <Square value={i} />;
+    return (
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
